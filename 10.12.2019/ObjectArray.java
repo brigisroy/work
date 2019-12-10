5917943d529cae58b8a93b7fe00c9aa017b4c61e
Brigis Roy <43792122+brigisroy@users.noreply.github.com>
2019-12-10 12:04:30 +0530

    Add files via upload

---
+++ 10.12.2019/ObjectArray.java
@@ -0,0 +1,54 @@
+package objectarray;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Brigis Roy
+ */
+class  data{
+    int num1,num2;
+   
+    
+    
+    public void get_data(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the 1st number :");
+        num1 = sc.nextInt();
+        System.out.print("enter the 2nd number :");
+        num2 = sc.nextInt();
+         
+                   
+    }
+     void display_data(){
+        System.out.println("the 1st Number is "+num1);
+        System.out.println("the 2nd Number is "+num2);
+        
+    }
+
+    
+}
+public class ObjectArray {
+         
+                      
+
+    public static void main(String[] args) {
+
+        data[] obj = new data[2];
+         obj[0]=new data();
+         obj[1]=new data();
+        
+        System.out.println("getting data for 1st object");
+        obj[0].get_data();
+        System.out.println("getting data for 1st object");
+        obj[1].get_data();
+        System.out.println("displaying data for object 1");
+        obj[0].display_data();
+        System.out.println("displaying data for object 2");
+        obj[1].display_data();
+
+        
+    }
+
+ 
+}
