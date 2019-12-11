8900ee243badd0549816747a7032c177602221f9
Brigis Roy <43792122+brigisroy@users.noreply.github.com>
2019-12-11 11:48:15 +0530

    Update LambdaExpressions.java

--- 11.12.2019/LambdaExpressions.java
+++ 11.12.2019/LambdaExpressions.java
@@ -10,21 +10,33 @@ interface reverseString{
 }
 public class LambdaExpressions {
     public static void main(String[] args) {
-        name a=(q,w)-> q+w; //for single line no return keyword is used  
+        name a=(q,w)-> q+w;
         System.out.println("addition using lambdaexpressions : "+a.add(20, 40));
-        String str2 = "hello";
+        String str2 = "lambdaexpressions";
         reverseString rev;
         rev = (str2)->
         {
+            String word="";
             int len=str2.length()-1;
             for(int i = len;i>=0;i--)
             {
-                System.out.print(str2.charAt(i));
+                word = word + (str2.charAt(i));
             }
-            return null; // must have retrun type otherwise the program will not halt
+            return word; // must have retrun type otherwise the program will not halt
         };
         //must terminate with terminater for milti line lambda Expressions and should return something or else use null
-           rev.reverse(str2);
+       
+        System.out.println("Reversing of string using lambda Expressions :"+rev.reverse(str2));
+        
+        
+        //lambda Expressions using ArrayList
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.forEach(n -> System.out.println(+n)); 
     }
     
 }
+
