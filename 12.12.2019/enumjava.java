9d068e294ee809e0ec299baac5e166caf0fa13a7
Brigis Roy <43792122+brigisroy@users.noreply.github.com>
2019-12-12 15:29:31 +0530

    Create enumjava.java

---
+++ 12.12.2019/enumjava.java
@@ -0,0 +1,12 @@
+
+package enumjava;
+enum animails{
+    dog,cat,horse,tiger
+}
+
+public class enumjava {
+    public static void main(String[] args) 
+    {
+        System.out.println(animails.dog);
+    }
+}
