e07ce484bc792d087ebcc35a0ff8a93d92751870
Brigis Roy <43792122+brigisroy@users.noreply.github.com>
2019-12-13 10:15:57 +0530

    Update java_super.java

--- 13.12.2019/java_super.java
+++ 13.12.2019/java_super.java
@@ -6,20 +6,23 @@ class car{
       car(){
          System.out.println("At base class "+color);
      }
+      void hello(){
+          System.out.println("Super Class");
+      }
 }
 class blackcar extends car{
     String color;
 
     blackcar() {
         this.color = "black";
-        // this keyword is used to define the value of color in the local block
-       // super();
+        // this keyword is used to define the value in the local block
+       
     }
     
     void print(){
     System.out.println("at inherted class "+color);
     System.out.println("at inherted class "+super.color);
-    
+    super.hello();
 }
     
 }
