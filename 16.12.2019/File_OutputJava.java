38c67e1c31308eb3bf283491e9ba7363028c601d
Brigis Roy <43792122+brigisroy@users.noreply.github.com>
2019-12-16 15:53:17 +0530

    Add files via upload

---
+++ 16.12.2019/File_OutputJava.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package file_output.java;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ *
+ * @author Brigis
+ */ 
+public class File_OutputJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        FileInputStream  fout = new FileInputStream("test.txt");
+        //object creatation for file Writting
+        int i =0;
+        while((i=fout.read())!=-1)
+            //
+        {
+        System.out.print((char)i);
+        }
+        System.out.println();
+        fout.close();     
+        
+    }
+    
+}
