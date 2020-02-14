2dd611fe21c246781875522dfd139bd1494956ca
Brigis Roy <43792122+brigisroy@users.noreply.github.com>
2020-02-14 15:19:24 +0530

    Add files via upload

---
+++ 20.02.2020/bike.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class bike implements vehicle {
+	public void drive() {
+		System.out.println("this from bike Class ");
+	}
+}
---
+++ 20.02.2020/car.java
@@ -0,0 +1,7 @@
+package com.test;
+
+public class car implements vehicle {
+	public void drive() {
+		System.out.println("this from car Class");
+	}
+}
